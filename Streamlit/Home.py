1d1694998fb1a4dd82e79bb1ca5e80bbe3e2cf37
eliBresnan <135745408+eliBresnan@users.noreply.github.com>
2025-05-12 06:10:57 -0500

    Update Home.py

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -1,10 +1,14 @@
+import os
 import streamlit as st
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+img_path = os.path.join(dir_path, "images","pbs-kids-gif-basketball.gif")
 
 st.title("Quantum Computing Final Presentation")
 st.subheader("A Quantum Nueral Network for Time Series Prediction")
 st.caption("By Elijah Bresnan")
 st.markdown("---")
-st.image("Streamlit\images\pbs-kids-gif-basketball.gif")
+st.image(img_path)
 
 if st.button("Next >>"):
     st.switch_page("pages/1_Project_Overview.py")
