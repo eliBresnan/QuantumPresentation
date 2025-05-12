4f23800a07082972e44e71ce27e76365431e8d36
eliBresnan <135745408+eliBresnan@users.noreply.github.com>
2025-05-12 06:01:31 -0500

    Update Home.py

--- Streamlit/Home.py
+++ Streamlit/Home.py
@@ -4,7 +4,7 @@ st.title("Quantum Computing Final Presentation")
 st.subheader("A Quantum Nueral Network for Time Series Prediction")
 st.caption("By Elijah Bresnan")
 st.markdown("---")
-st.image("images\pbs-kids-gif-basketball.gif")
+st.image("Streamlit\images\pbs-kids-gif-basketball.gif")
 
 if st.button("Next >>"):
     st.switch_page("pages/1_Project_Overview.py")
